dcfd703e2fa352f3dc41f6f9485f731dee6fa4c9
pesarlanka devendrababu <devendrababu9666@gmail.com>
2023-08-24 20:09:57 +0530

    mongodb string changed

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://learning:DcGtmPHw7kyKveNv@cluster0.t6fgp1f.mongodb.net/");
+    await mongoose.connect("mongodb+srv://devendrababu9666:pJy0JAgUB3J3fN6d@cluster0.6hoqhdc.mongodb.net/");
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`Mongodb Server Issue ${error}`.bgRed.white);
